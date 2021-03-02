8d5bc69c802d6fe107ad7a3c62b3f8b5c32d913c
Aram2142 <andresaaramirez@aol.com>
2021-03-01 23:55:51 -0500

    took out animation effect

--- clock.js
+++ clock.js
@@ -1,7 +1,6 @@
 //make the code for the clock here
 //get hours, minutes, seconds
 const clock = document.getElementById('time');
-let timeValue;
 //get time methods
 
 function makeTwoNumbers(num) {
@@ -29,8 +28,10 @@ const runClock = () => {
     let AmOrPM = hours >= 12 ? 'pm' : 'am';
     hours = (hours % 12) || 12; //works cited: https://medium.com/front-end-weekly/how-to-convert-24-hours-format-to-12-hours-in-javascript-ca19dfd7419d
 
+    
+
     clock.innerHTML = makeTwoNumbers(hours) + ": " + makeTwoNumbers(minutes) + ": " + makeTwoNumbers(seconds) + AmOrPM;
-    let time = setInterval(runClock, 500)//worsk cited: w3 schools mini timing clock
+    let time = setInterval(runClock, 500)
 }
 runClock()
 
